675ea9b6192986a595a15c6a82f251be62bdead1
schooolman <j.schoolmee@gmail.com>
2016-07-20 23:03:48 -0500

    Last update for the evening, began the IOS path on codeschool. Notes and basics here

--- helloWorld.m
+++ helloWorld.m
@@ -1,3 +1,22 @@
 NSLog(@"Hello world!");
 NSLog(@"I'm here to begin learning some Objective C");
 NSLog(@"Hopefully I will be able to transition this into learning Swift and learn more about IOS Development!");
+
+// NOTES
+NSString *firstName = @"Jake";
+NSString *lastName = @"Schoolmeesters";
+NSNumber *age = @29;
+
+// Using NSLog to pass variables
+NSLog(@ "%@ is %@ years old", firstName, age);
+
+
+// Array
+NSArray *colors = @[@"red", @"blue", @"green", @"yellow"];
+// Array's in Objective C are immutable objects
+
+
+// NSDictionary
+NSDictionary *jakeRating = @{@"awesomeRating": @1000, @"coolnessRating": @2000000};
+
+// This was just the first lesson on codeschool in the IOS development course. I plan to work my way through any of the online courses that I can find for IOS development, and then will move onto the stanford free courses that they offer for IOS development and begin to research possible books that would be a good way to start to supplement learning. So it begins!
