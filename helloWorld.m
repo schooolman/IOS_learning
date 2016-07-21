dd408807b9b51076360fd26e4db5f546ce3cf94f
schooolman <j.schoolmee@gmail.com>
2016-07-20 22:18:39 -0500

    updating my intention with starting by learning objective C

--- helloWorld.m
+++ helloWorld.m
@@ -1,1 +1,3 @@
 NSLog(@"Hello world!");
+NSLog(@"I'm here to begin learning some Objective C");
+NSLog(@"Hopefully I will be able to transition this into learning Swift and learn more about IOS Development!");
