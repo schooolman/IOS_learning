455373e157ea7683f4ccd5c2373fea0b90856fa2
schooolman <j.schoolmee@gmail.com>
2016-07-21 23:34:17 -0500

    updated notes and experiments

--- 7_21_16_ObjectiveC_Learning.m
+++ 7_21_16_ObjectiveC_Learning.m
@@ -10,3 +10,31 @@ NSArray *foods = @[@"tacos", @"burgers"];
 NSString *result = @[foods description];
 
 NSLog(@"%@", result);
+
+// Some variables need an asterik and some dont. ugh.
+
+// %@ is just one of many format specifiers aka "placeholders" that can be used in NSLog and other data types	
+// To log a NSUInteger use %lu in place of %@
+
+// Note: A good rule of thumb for how to tell if an object is an Objective-C object or a C one: Objective-C objects are defined with the * in front of variable names, like this: NSString *name
+
+
+// figure out my dogs age in dog years
+
+// declaring variables
+NSNumber *tilly = @3;
+NSNumber *dogYears = @7;
+
+// convert to unsigned integers
+NSUInteger tillyDog = [tilly unsignedIntegerValue];
+NSUInteger dogMultiplyer = [dogYears unsignedIntegerValue];
+
+// multiply
+NSUInteger tillyAge = tillyDog * dogMultiplyer;
+
+// log out the results
+NSLog(@"My dog Tilly is %lu years old....in dog years", tillyAge);
+
+
+
+
