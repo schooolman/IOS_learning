82a17462b337e03f666d5cf9aec1e0e8019f9bdb
schooolman <j.schoolmee@gmail.com>
2016-07-21 23:11:42 -0500

    July 21st short coding session, level two objective C training

---
+++ 7_21_16_ObjectiveC_Learning.m
@@ -0,0 +1,12 @@
+// Objective C and sending Messages
+
+// There are objects built into Objective C such as description
+
+// passing the description to the food array
+NSArray *foods = @[@"tacos", @"burgers"];
+
+// NSLog(@"%@", [foods description]);
+
+NSString *result = @[foods description];
+
+NSLog(@"%@", result);
