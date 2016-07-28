62a609087d4a53dc7f681525b9f2d085da47450f
schooolman <j.schoolmee@gmail.com>
2016-07-27 23:26:07 -0500

    one more chapter in the objective C road

--- 7_21_16_ObjectiveC_Learning.m
+++ 7_21_16_ObjectiveC_Learning.m
@@ -190,6 +190,40 @@ for (NSString *hat in newHats) {
   }
 }
 
+NSDictionary *funnyWords = @{
+  @"Schadenfreude": @"pleasure derived by someone from another person's misfortune.",
+  @"Portmanteau": @"consisting of or combining two or more separable aspects or qualities",
+  @"Penultimate": @"second to the last"
+};
+
+for (NSString *word in funnyWords){
+  NSString *definition = funnyWords[word];
+  NSLog(@"%@ is defined as %@", word, definition);
+}
+
+NSDictionary *funnyWords = @{
+  @"Schadenfreude": @"pleasure derived by someone from another person's misfortune.",
+  @"Portmanteau": @"consisting of or combining two or more separable aspects or qualities",
+  @"Penultimate": @"second to the last"
+};
+
+for (NSString *word in funnyWords) {
+  NSString *definition = funnyWords[word];
+  NSLog(@"%@ is defined as %@", word, definition);
+}
+
+
+NSDictionary *funnyWords = @{
+  @"Schadenfreude": @"pleasure derived by someone from another person's misfortune.",
+  @"Portmanteau": @"consisting of or combining two or more separable aspects or qualities",
+  @"Penultimate": @"second to the last"
+};
+
+for (NSString *word in funnyWords){
+  NSString *definition = funnyWords[word];
+  NSLog(@"%@ is defined as %@", word, definition);
+}
+
 
 
 
