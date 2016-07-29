e92cc3764a604851cc6a180198ded3d40baf8696
schooolman <j.schoolmee@gmail.com>
2016-07-29 17:08:13 -0500

    starting level four of Objective C on code school.

---
+++ Chapter4_ObjectiveC.m
@@ -0,0 +1,24 @@
+// Creating a class in Objective C
+// Classes start with an uppercase letter
+// Objects start with a lower case letter
+// 
+// Defining it's interface
+// @interface SillyIphone : NSObject
+// @end
+
+// You can write properties of the class with @property NSString *firstName; @property NSString *lastName;
+// THis goes between the @interface and the @end
+
+// @interface SillyIphone : NSObject
+// @property NSString *phoneName;
+// @property NSString *modelNumber;
+// @end
+
+
+// Seperate out the class from the main file, and put it into a header file. The header file ends with a .h extension.
+#import "SillyIphone.h"
+
+// implenting the class below
+@implementation SillyIphone
+@end
+
