4befec567bac258098b9ae5cd0b7a982ea09ea15
schooolman <j.schoolmee@gmail.com>
2016-08-03 21:56:09 -0500

    getting started with xCode

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "check this shit out!!"
+6+3
+77*2
+
+var newVariable = "store strings here, easy math and output on side. CRAZY!"
+
+var str: String = "This is setting a variable"
+
+var age: Int = 29
