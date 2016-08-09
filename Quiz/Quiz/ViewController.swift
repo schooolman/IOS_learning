e2e310770d38ae7ba5e44564a881de848f342982
schooolman <j.schoolmee@gmail.com>
2016-08-08 22:32:59 -0500

    finished my first ios app in swift. Its basic as all hell but was fun to start doing some actual app dev stuffs

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -12,12 +12,39 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    
+//  let is used with the string arrays because they will not change, and should not change.
+    let questions: [String] = ["From what is cognac made?",
+                               "what is 7+7",
+                               "what is the capital of Vermont?"]
+    
+    let answers: [String] = ["Grapes",
+                             "14",
+                             "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(sender: AnyObject) {
+        ++currentQuestionIndex
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
         
     }
     
     @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
         
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
     }
 
 }
