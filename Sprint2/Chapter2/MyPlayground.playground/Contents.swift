55a9573e06b23bb6e730516422367afbf403c9b5
schooolman <j.schoolmee@gmail.com>
2016-08-17 23:14:06 -0500

    Starting chapter 2 of Nerd Ranch iOS book.

---
+++ Sprint2/Chapter2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+var chapterTwo = true
+
+//Starting chapter 2 of the Big Nerd Ranch guide to iOS development, using this doc to take notes and work.
+
+//types in Swift
+//Properties: values associated with a type
+//initializers: code that initializes an instance of a type
+//instance methods: functions specific to a type that can be called on an instance of that type
+//class or static methods: functions specific to a type that can be called on the type itself
+
+var str = "Hello, Swift"
+
+//let is used for constants. variables that you don't intend to change
+
+let constStr = str
+
+//type inference is means the compiler is inferring the type of the variable based on it's initial value
+//using option click will reveal QuickHelp to give you a further look into what is going on in the code behind the scenes
+
+
+//Specify the type in the declaration if you want to make sure it is set to a certain type or it doesn't have an initial value
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+
+//var arrayOfInts: Array<Int>
+var arrayOfInts: [Int]
+
+
+//Dictionaries are keys and values as an unordered collection. Values can be of any type, but they must be unique
+//var dictionaryOfCapitalsByCountry: Dictionary<String,String>
+var dictionaryOfCapitalsByCountry: [String:String]
+
+
---
+++ Sprint2/Chapter2/chapter_2.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+var chapterTwo = true
+
+//Starting chapter 2 of the Big Nerd Ranch guide to iOS development, using this doc to take notes and work.
+
+//types in Swift
+//Properties: values associated with a type
+//initializers: code that initializes an instance of a type
+//instance methods: functions specific to a type that can be called on an instance of that type
+//class or static methods: functions specific to a type that can be called on the type itself
+
+var str = "Hello, Swift"
+
+//let is used for constants. variables that you don't intend to change
+
+let constStr = str
+
+//type inference is means the compiler is inferring the type of the variable based on it's initial value
+//using option click will reveal QuickHelp to give you a further look into what is going on in the code behind the scenes
+
+
+
+
+
+
+
