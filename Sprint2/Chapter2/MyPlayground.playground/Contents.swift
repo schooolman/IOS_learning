9ad1d9b7d988e0b67fdc1e4f33222f85b3149a0a
schooolman <j.schoolmee@gmail.com>
2016-08-23 23:29:42 -0500

    working through chapter 2 in the Big Nerd Ranch iOS book

--- Sprint2/Chapter2/MyPlayground.playground/Contents.swift
+++ Sprint2/Chapter2/MyPlayground.playground/Contents.swift
@@ -10,11 +10,11 @@ var chapterTwo = true
 //instance methods: functions specific to a type that can be called on an instance of that type
 //class or static methods: functions specific to a type that can be called on the type itself
 
-var str = "Hello, Swift"
+//var str = "Hello, Swift"
 
 //let is used for constants. variables that you don't intend to change
 
-let constStr = str
+//let constStr = str
 
 //type inference is means the compiler is inferring the type of the variable based on it's initial value
 //using option click will reveal QuickHelp to give you a further look into what is going on in the code behind the scenes
@@ -32,5 +32,32 @@ var arrayOfInts: [Int]
 //Dictionaries are keys and values as an unordered collection. Values can be of any type, but they must be unique
 //var dictionaryOfCapitalsByCountry: Dictionary<String,String>
 var dictionaryOfCapitalsByCountry: [String:String]
+
+
+//Sets are similar to arrays but are unordered. Members must be unique as well as hashable. 
+
+var winningLotteryNumbers: Set<Int>
+
+//Standard types can be assigned literal values, or literals.
+
+var str = "Hello, Playground"
+
+str = "hello, swift"
+
+let constStr = str
+
+let number = 42
+let fmStation = 91.1
+
+//Arrays and dictionaries can be assigned literal values as well. Syntax resembles shorthand syntax for specifying these types
+
+let countinUp = ["one", "two"]
+let nameByParkingSpace = [13: "alice", 27: "Bob"]
+
+let countingUp =  ["one", "two"]
+let secondElement = countingUp[1]
+
+secondElement
+
 
 
