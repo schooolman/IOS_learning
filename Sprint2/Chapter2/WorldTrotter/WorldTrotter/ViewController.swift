0ba064782b6491afc8c8af87c27939407ea5013b
schooolman <j.schoolmee@gmail.com>
2016-08-27 18:08:02 -0500

    added labels and resized in xCode ui editor

--- Sprint2/Chapter2/WorldTrotter/WorldTrotter/ViewController.swift
+++ Sprint2/Chapter2/WorldTrotter/WorldTrotter/ViewController.swift
@@ -10,20 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blueColor()
-        view.addSubview(firstView)
-        
-        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.greenColor()
-//        view.addSubview(secondView)
-        firstView.addSubview(secondView)
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blueColor()
+//        view.addSubview(firstView)
+//        
+//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.greenColor()
+////        view.addSubview(secondView)
+//        firstView.addSubview(secondView)
+//    }
+//
 
 }
 
