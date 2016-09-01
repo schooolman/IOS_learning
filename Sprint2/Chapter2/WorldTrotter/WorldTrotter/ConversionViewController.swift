306668e447fa5b3b8eb6330a97504602e3c7102c
schooolman <j.schoolmee@gmail.com>
2016-08-31 23:51:54 -0500

    added a function to dismiss keyboard on background tap of world trotter app using the UITAPGestureRecognizer dots are starting to connect

--- Sprint2/Chapter2/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ Sprint2/Chapter2/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -11,9 +11,22 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     
+    
+    
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
     
     @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
-        celsiusLabel.text = textField.text
+//        celsiusLabel.text = textField.text
+        if let text = textField.text where !text.isEmpty {
+            celsiusLabel.text = text
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
     }
 }
